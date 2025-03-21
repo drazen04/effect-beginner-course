dae027c5a24f197faffa62ecec5001c8d8ca79a3
drazen04 <hangtime23@hotmail.it>
2025-03-21 21:50:29 +0100

    add not workin savePokemon api + use pipe from effect

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,15 @@ const fetchRequest = Effect.tryPromise(() =>
   fetch("https://pokeapi.co/api/v2/pokemon/garchomp/")
 );
 const jsonResponse = (response: Response) =>
-          Effect.tryPromise(() => response.json()); 
+  Effect.tryPromise(() => response.json()); 
 
-const main = Effect.flatMap(fetchRequest, jsonResponse)
+const savePokemon = (pokemon: unknown) =>
+  Effect.tryPromise(() => fetch("/api/pokemon", {body: JSON.stringify(pokemon)}))
+
+const main = fetchRequest.pipe(
+  Effect.flatMap(jsonResponse),
+  Effect.flatMap(savePokemon),
+)
 
 Effect.runPromise(main).then(console.log)
 
