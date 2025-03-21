3515e320f31cecfb694dcd09d3459b5b7240b42e
drazen04 <hangtime23@hotmail.it>
2025-03-21 18:12:37 +0100

    create a canonical fetch example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { Console, Effect } from "effect";
-
-const main = Console.log("Hello Effect");
-
-Effect.runSync(main)
+const main = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/garchomp/");
+    const json = await response.json();
+    return json;
+  };
+  
+  main().then(console.log);
