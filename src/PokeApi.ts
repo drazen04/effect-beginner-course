0a6a14cd6c0d4061fcf197feabd158e39ef1b9a6
drazen04 <hangtime23@hotmail.it>
2025-03-23 15:27:54 +0100

    extract shape and remove interface

--- src/PokeApi.ts
+++ src/PokeApi.ts
@@ -1,16 +1,39 @@
-import { Config, Context, Effect, Schema, type ParseResult } from "effect";
+import { Context, Effect, Schema } from "effect";
 import { Pokemon } from "./schemas";
 import { FetchError, JsonError } from "./errors";
-import type { ConfigError } from "effect/ConfigError";
 import { PokemonCollection } from "./PokemonCollection";
 import { BuildPokeApiUrl } from "./BuildPokeApiUrl";
 
-interface PokeApiImpl {
-    readonly getPokemon: Effect.Effect<
-        Pokemon,
-        FetchError | JsonError | ParseResult.ParseError | ConfigError,
-        PokemonCollection | BuildPokeApiUrl
-    >;
+/**
+ * extract implementation is very useful to avoiding sync interface
+ * and implementation itself.
+ * So now we can use "typeof make" as shape declararion and only "make" as concrete implementation 
+ */
+const make = {
+    getPokemon: Effect.gen(function* () {
+        const pokemonCollection = yield* PokemonCollection;
+        const buildPokeApiUrl = yield* BuildPokeApiUrl;
+        
+        const requestUrl = buildPokeApiUrl({
+            name: pokemonCollection[0]
+        })
+        
+        const response = yield* Effect.tryPromise({
+            try: () => fetch(requestUrl),
+            catch: () => new FetchError()
+        });
+        
+        if (!response.ok) {
+            return yield* new FetchError();
+        }
+        
+        const json = yield* Effect.tryPromise({
+            try: () => response.json(),
+            catch: () => new JsonError()
+        });
+        
+        return yield* Schema.decodeUnknown(Pokemon)(json);
+    })
 }
 
 /**
@@ -20,31 +43,6 @@ interface PokeApiImpl {
  * - having only one declaration/value
  * - collect all implementation inside the class as static member such as Live-Test-Mock-Dev
  */
-export class PokeApi extends Context.Tag("PokeApi")<PokeApi, PokeApiImpl>() {
-    static readonly Live = PokeApi.of({
-        getPokemon: Effect.gen(function* () {
-            const pokemonCollection = yield* PokemonCollection;
-            const buildPokeApiUrl = yield* BuildPokeApiUrl;
-            
-            const requestUrl = buildPokeApiUrl({
-                name: pokemonCollection[0]
-            })
-            
-            const response = yield* Effect.tryPromise({
-                try: () => fetch(requestUrl),
-                catch: () => new FetchError()
-            });
-            
-            if (!response.ok) {
-                return yield* new FetchError();
-            }
-            
-            const json = yield* Effect.tryPromise({
-                try: () => response.json(),
-                catch: () => new JsonError()
-            });
-            
-            return yield* Schema.decodeUnknown(Pokemon)(json);
-        })
-    })
+export class PokeApi extends Context.Tag("PokeApi")<PokeApi, typeof make>() {
+    static readonly Live = PokeApi.of(make)
 }
