15efda7174703dd493dcb97cb103d9f0f51bf82b
drazen04 <hangtime23@hotmail.it>
2025-03-23 15:06:50 +0100

    create Live impl for PokemonCollection

--- src/PokemonCollection.ts
+++ src/PokemonCollection.ts
@@ -3,4 +3,6 @@ import { Array, Context } from "effect";
 export class PokemonCollection extends Context.Tag("PokemonCollection")<
     PokemonCollection,
     Array.NonEmptyArray<string>
->() {}
+>() {
+    static readonly Live = PokemonCollection.of(["staryu", "perrserker", "flaaffy"])
+}
