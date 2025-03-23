94a3b37dbe1d96d64746fb680234e79d5c59bfc8
drazen04 <hangtime23@hotmail.it>
2025-03-23 15:18:32 +0100

    use Services as dependency in getPokemon

--- src/PokeApi.ts
+++ src/PokeApi.ts
@@ -2,6 +2,8 @@ import { Config, Context, Effect, Schema, type ParseResult } from "effect";
 import { Pokemon } from "./schemas";
 import { FetchError, JsonError } from "./errors";
 import type { ConfigError } from "effect/ConfigError";
+import { PokemonCollection } from "./PokemonCollection";
+import { BuildPokeApiUrl } from "./BuildPokeApiUrl";
 
 interface PokeApiImpl {
     readonly getPokemon: Effect.Effect<
@@ -20,23 +22,28 @@ interface PokeApiImpl {
 export class PokeApi extends Context.Tag("PokeApi")<PokeApi, PokeApiImpl>() {
     static readonly Live = PokeApi.of({
         getPokemon: Effect.gen(function* () {
-          const baseUrl = yield* Config.string("BASE_URL");
-        
-          const response = yield* Effect.tryPromise({
-            try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
-            catch: () => new FetchError()
-          });
-        
-          if (!response.ok) {
-            return yield* new FetchError();
-          }
-        
-          const json = yield* Effect.tryPromise({
-            try: () => response.json(),
-            catch: () => new JsonError()
-          });
-        
-          return yield* Schema.decodeUnknown(Pokemon)(json);
+            const pokemonCollection = yield* PokemonCollection;
+            const buildPokeApiUrl = yield* BuildPokeApiUrl;
+            
+            const requestUrl = buildPokeApiUrl({
+                name: pokemonCollection[0]
+            })
+            
+            const response = yield* Effect.tryPromise({
+                try: () => fetch(requestUrl),
+                catch: () => new FetchError()
+            });
+            
+            if (!response.ok) {
+                return yield* new FetchError();
+            }
+            
+            const json = yield* Effect.tryPromise({
+                try: () => response.json(),
+                catch: () => new JsonError()
+            });
+            
+            return yield* Schema.decodeUnknown(Pokemon)(json);
         })
     })
 }
