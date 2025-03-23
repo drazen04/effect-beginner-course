355990630d78d49f44fe517168246bb7e27a0396
drazen04 <hangtime23@hotmail.it>
2025-03-23 19:14:38 +0100

    use ManagedRuntime instead of pipe program with Effect.provide

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
-import { Effect, Layer } from "effect";
+import { Effect, Layer, ManagedRuntime } from "effect";
 import { PokeApi } from "./PokeApi";
 
 const MainLayer = Layer.mergeAll(PokeApi.Default)
+
+/**
+ * Runtime is convient collect Layers and run the program without using Effect.provide
+ */
+const PokemonRuntime = ManagedRuntime.make(MainLayer)
 
 const program = Effect.gen(function* () {
   const pokeApi = yield* PokeApi;
   return yield* pokeApi.getPokemon;
 })
 
-const runnable = program.pipe(Effect.provide(MainLayer))
+// const runnable = program.pipe(Effect.provide(MainLayer))
 
-const main = runnable.pipe(
+const main = program.pipe(
   Effect.catchTags({
     "FetchError": () => Effect.succeed("Fetch Error"),
     "JsonError": () => Effect.succeed("Json Error"),
@@ -18,7 +23,7 @@ const main = runnable.pipe(
   })
 )
 
-Effect.runPromise(main).then(console.log)
+PokemonRuntime.runPromise(main).then(console.log)
 
 // Eager version
 // const main = async () => {
